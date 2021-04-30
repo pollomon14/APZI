3b3904250d567cc046db1c640f8ab4f70bf9580a
pollomon14 <pollomon14@hotmail.com>
2021-04-29 22:16:26 -0500

    directorio

--- app/Http/Controllers/FrontdirectorioController.php
+++ app/Http/Controllers/FrontdirectorioController.php
@@ -5,6 +5,7 @@ use App\Models\directorio;
 use App\Models\iconos;
 use App\Models\eventos;
 use App\Models\municipios;
+use App\Models\subcategorias;
 use Illuminate\Http\Request;
 
 class FrontdirectorioController extends Controller
@@ -13,9 +14,10 @@ class FrontdirectorioController extends Controller
     {
         $directorio = directorio::where('id_municipio',$var)->get();
         $iconos = iconos::orderBy('id', 'ASC')->get();
+        $subcategorias = subcategorias::orderBy('id', 'ASC')->get();
         $eventos = eventos::where('id_municipio',$var)->get();
         $municipio = municipios::find($var);
-        return view('front.directorio',compact('directorio','iconos','eventos','municipio'));
+        return view('front.directorio',compact('directorio','iconos','eventos','municipio','subcategorias'));
 
     }
 
--- resources/views/front/directorio.blade.php
+++ resources/views/front/directorio.blade.php
@@ -102,23 +102,27 @@
 
 
     <!-- End: 4 Col Small Slider -->
-    @foreach($directorio as $dir)
-        <h6>{{ $dir->nombre }}</h6>
-    @endforeach
 
-    <h1
-        style="width: 94%;margin-left: 3%;color: #000c4f;font-size: 30px;font-family: 'ITC Avant Garde Gothic Std Medium';border-bottom: 2px solid #000c4f;line-height: 27px;">
+
+    <h1 style="width: 94%;margin-left: 3%;color: #000c4f;font-size: 30px;font-family: 'ITC Avant Garde Gothic Std Medium';border-bottom: 2px solid #000c4f;line-height: 27px;">
         A</h1><!-- Start: divider-text-middle -->
     <div style="text-align:center;"></div><!-- End: divider-text-middle -->
+    @foreach($directorio as $dir)
+    @foreach($subcategorias as $subcategoria)
+    @foreach($iconos as $icono)
     <!-- Start: 1 Row 2 Columns -->
+    @if ($dir->tipo_de_plan === 4)
     <div style="text-align: center;margin-top: 5%;margin-bottom: 5%;">
         <div class="container">
             <div class="row"
                 style="width: 98%;margin-left: 1%;margin-right: 0;border: 1px solid #000c4f;border-radius: 4px;">
                 <div class="col-md-6" style="width: 30%;padding-right: 0;padding-left: 0;">
                     <div class="row d-flex align-items-center" style="height: 70%;">
+                    @if ($dir->logo === $icono->id)
                         <div class="col"><img style="width: 100%;border-radius: 10px;"
-                                src="/assets/img/logo_garantia.jpg?h=25de05e105d08715f09c3253012b6834"></div>
+                                src="{{ $icono->link }}"></div><h1>hola</h1>
+                    @break
+                    @endif            
                     </div>
                     <div class="row" style="height: 25%;margin-right: 0;margin-left: 0;">
                         <div class="col"><i class="fa fa-eject rubberBand animated infinite"
@@ -132,17 +136,20 @@
                     </div>
                 </div>
                 <div class="col-md-6" style="width: 70%;">
+                @if ($dir->id_subcategoria === $subcategoria->id)
                     <div class="row">
                         <div class="col" style="border-bottom: 1px solid #000c4f ;border-left: 1px solid #000c4f ;"><span
-                                style="font-family: 'Montserrat Regular';color: #000c4f;font-size: 14px;">Zapatería</span>
+                                style="font-family: 'Montserrat Regular';color: #000c4f;font-size: 14px;">{{ $subcategoria->nombre }}</span>
                         </div>
                     </div>
+                    @break
+                    @endif 
                     <div class="row"
                         style="border-bottom: 1px solid #000c4f;border-left: 1px solid #000c4f;background: #ffff00;">
                         <div class="col" style="width: 100%;padding-right: 0;padding-left: 0;">
                             <h1
                                 style="width: 90%;font-size: 14px;color: #000c4f;font-family: 'ITC Avant Garde Gothic Std Medium';line-height: 16px;margin-bottom: 0;text-align: center;margin-left: 5%;">
-                                <br>REMONTADORA DE CALZADO LA GARANTÍA<br><br></h1>
+                                <br>{{ $dir->nombre }}<br><br></h1>
                         </div>
                     </div>
                     <div class="row">
@@ -150,7 +157,7 @@
                             style="border-bottom: 1px solid #000c4f;border-left: 1px solid #000c4f;padding-right: 0;padding-left: 0;">
                             <h1
                                 style="width: 100%;font-size: 14px;font-family: 'Montserrat Regular';color: #000c4f;font-weight: normal;margin-bottom: 5%;">
-                                <br>Calle 11 # 12-17 Liberia Piso 1<br></h1>
+                                <br>{{ $dir->direccion }}<br></h1>
                         </div>
                     </div>
                     <div class="row" style="background: #f2f2f2;">
@@ -161,7 +168,7 @@
                                 style="margin-left: 5%;color: #000c4f;"></i>
                             <h1
                                 style="font-size: 14px;width: 100%;color: #000c4f;line-height: 6px;font-family: 'ITC Avant Garde Gothic Std Medium';">
-                                <br>314 448 3232<br><br></h1>
+                                <br>{{ $dir->telefono }}<br><br></h1>
                         </div>
                         <div class="col" style="width: 50%;padding-left: 0;padding-right: 0;"><i
                                 class="fa fa-angle-double-right" style="margin-right: 5%;color: #000c4f;"></i><i
@@ -169,7 +176,7 @@
                                 class="fa fa-angle-double-left" style="margin-left: 5%;color: #000c4f;"></i>
                             <h1
                                 style="font-size: 14px;width: 100%;color: #000c4f;line-height: 6px;font-family: 'ITC Avant Garde Gothic Std Medium';">
-                                <br>314 448 3232<br><br></h1>
+                                <br>{{ $dir->whatsapp }}<br><br></h1>
                         </div>
                     </div>
                     <div class="row">
@@ -182,17 +189,18 @@
             </div>
         </div>
     </div><!-- End: 1 Row 2 Columns -->
+    @elseif ($dir->tipo_de_plan === 3)
     <!-- Start: 1 Row 2 Columns -->
     <div style="text-align: center;margin-top: 5%;margin-bottom: 5%;">
         <div class="container">
             <div class="row"
                 style="width: 98%;margin-left: 1%;margin-right: 0;border: 1px solid #000c4f;border-radius: 4px;">
                 <div class="col-md-6 d-flex align-items-center" style="width: 30%;padding-right: 0;padding-left: 0;"><img
-                        style="width: 100%;" src="/assets/img/logo_garantia.jpg?h=25de05e105d08715f09c3253012b6834"></div>
+                        style="width: 100%;" src="{{ $dir->logo }}"></div>
                 <div class="col-md-6" style="width: 70%;">
                     <div class="row">
                         <div class="col" style="border-bottom: 1px solid #000c4f ;border-left: 1px solid #000c4f ;"><span
-                                style="font-family: 'Montserrat Regular';color: #000c4f;font-size: 14px;">Zapatería</span>
+                                style="font-family: 'Montserrat Regular';color: #000c4f;font-size: 14px;">{{ $subcategoria->nombre }}</span>
                         </div>
                     </div>
                     <div class="row"
@@ -200,7 +208,7 @@
                         <div class="col" style="width: 100%;padding-right: 0;padding-left: 0;">
                             <h1
                                 style="width: 90%;font-size: 14px;color: #000c4f;font-family: 'ITC Avant Garde Gothic Std Medium';line-height: 16px;margin-bottom: 0;text-align: center;margin-left: 5%;">
-                                <br>REMONTADORA DE CALZADO LA GARANTÍA<br><br></h1>
+                                <br>{{ $dir->nombre }}<br><br></h1>
                         </div>
                     </div>
                     <div class="row">
@@ -208,7 +216,7 @@
                             style="border-bottom: 1px solid #000c4f;border-left: 1px solid #000c4f;padding-right: 0;padding-left: 0;">
                             <h1
                                 style="width: 100%;font-size: 14px;font-family: 'Montserrat Regular';color: #000c4f;font-weight: normal;margin-bottom: 5%;">
-                                <br>Calle 11 # 12-17 Liberia Piso 1<br></h1>
+                                <br>{{ $dir->direccion }}<br></h1>
                         </div>
                     </div>
                     <div class="row" style="background: #f2f2f2;">
@@ -218,7 +226,7 @@
                                 style="font-size: 14px;width: 100%;color: #000c4f;line-height: 6px;font-family: 'ITC Avant Garde Gothic Std Medium';margin-bottom: 0;padding-bottom: 1%;padding-top: 1%;">
                                 <br><i class="fa fa-angle-double-right" style="color: #000c4f;margin-right: 2%;"></i><i
                                     class="fa fa-phone" style="color: #000c4f;"></i><i class="fa fa-angle-double-left"
-                                    style="color: #000c4f;margin-left: 2%;"></i>&nbsp; 314 448 3232<br><br></h1>
+                                    style="color: #000c4f;margin-left: 2%;"></i>&nbsp; {{ $dir->telefono }}<br><br></h1>
                         </div>
                     </div>
                     <div class="row">
@@ -231,6 +239,7 @@
             </div>
         </div>
     </div><!-- End: 1 Row 2 Columns -->
+    @elseif ($dir->tipo_de_plan === 1)
     <!-- Start: 1 Row 2 Columns -->
     <div style="text-align: center;margin-top: 5%;margin-bottom: 5%;">
         <div class="container">
@@ -239,7 +248,7 @@
                 <div class="col-md-6" style="width: 100%;">
                     <div class="row">
                         <div class="col" style="border-bottom: 1px solid #000c4f ;border-left: 1px none #000c4f ;"><span
-                                style="font-family: 'Montserrat Regular';color: #000c4f;font-size: 14px;">Zapatería</span>
+                                style="font-family: 'Montserrat Regular';color: #000c4f;font-size: 14px;">{{ $subcategoria->nombre }}</span>
                         </div>
                     </div>
                     <div class="row"
@@ -247,7 +256,7 @@
                         <div class="col" style="width: 100%;padding-right: 0;padding-left: 0;border-left-style: none;">
                             <h1
                                 style="width: 90%;font-size: 14px;color: #000c4f;font-family: 'ITC Avant Garde Gothic Std Medium';line-height: 16px;margin-bottom: 0;text-align: center;margin-left: 5%;">
-                                <br>REMONTADORA DE CALZADO LA GARANTÍA<br><br></h1>
+                                <br>{{ $dir->nombre }}<br><br></h1>
                         </div>
                     </div>
                     <div class="row">
@@ -255,7 +264,7 @@
                             style="padding-right: 0;padding-left: 0;border-bottom: 1px solid #000c4f ;border-left: 1px none #000c4f ;">
                             <h1
                                 style="width: 100%;font-size: 14px;font-family: 'Montserrat Regular';color: #000c4f;font-weight: normal;margin-bottom: 5%;">
-                                <br>Calle 11 # 12-17 Liberia Piso 1<br></h1>
+                                <br>{{ $dir->direccion }}<br></h1>
                         </div>
                     </div>
                     <div class="row"
@@ -266,11 +275,16 @@
                                 style="font-size: 14px;width: 100%;color: #000c4f;line-height: 6px;font-family: 'ITC Avant Garde Gothic Std Medium';margin-bottom: 0;padding-top: 1%;padding-left: 1%;">
                                 <br><i class="fa fa-angle-double-right" style="color: #000c4f;margin-right: 2%;"></i><i
                                     class="fa fa-phone" style="color: #000c4f;"></i><i class="fa fa-angle-double-left"
-                                    style="color: #000c4f;margin-left: 2%;"></i>&nbsp; 314 448 3232<br><br></h1>
+                                    style="color: #000c4f;margin-left: 2%;"></i>&nbsp; {{ $dir->telefono }}<br><br></h1>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div><!-- End: 1 Row 2 Columns -->
+
+            @endif
+            @endforeach
+            @endforeach
+            @endforeach
 @endsection
